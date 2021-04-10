2f1886dfb527d6495b9c5dca2dad9745bdccd800
Fedulov Anton P <live.stone.anton@gmail.com>
2021-04-10 23:25:31 +0500

    init 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { ScullyLibModule } from '@scullyio/ng-lib';
 import { RouterModule } from '@angular/router';
 
 import { Routes } from '@angular/router';
+import { LogoComponent } from './components/logo/logo.component';
 
 export const routes: Routes = [];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LogoComponent],
   imports: [BrowserModule, ScullyLibModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
